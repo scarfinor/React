99e6624758535d3117c462aa442b6c3388d92ee3
scarfinor <scarfinor@gmail.com>
2024-08-14 21:13:05 -0500

    git commit First Commit chapter 29 directory

---
+++ Chapter 29/app.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <SchnucksInfo />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ Chapter 29/components/SchnucksInfo.jsx
@@ -0,0 +1,9 @@
+function SchnucksInfo() {
+  return (
+    <div>
+      <h1>Schnucks</h1>
+      <p>Grocery store</p>
+      <p>Delivering within an hour</p>
+    </div>
+  );
+}
